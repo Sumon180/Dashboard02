74312e02012b9042c92798ff9310531b5248eba2
sumon-hossain <sumonengr.96@gmail.com>
2023-03-12 16:50:47 +0600

    WIP

--- src/pages/Email.tsx
+++ src/pages/Email.tsx
@@ -9,108 +9,102 @@ const Email = () => {
 
   return (
     <>
-      <div className="">
-        <div className="flex overflow-x-hidden">
+      <div className="flex overflow-x-hidden">
+        <div
+          className={`fixed  w-60 lg:block hidden transition-all duration-300 h-screen border-r border-gray-400 ${
+            mode ? "bg-black text-slate-300" : "bg-white text-slate-800"
+          }`}
+        >
+          <Sidebar />
+        </div>
+        <div className="lg:ml-60 ml-0  w-[85.7vw]">
+          <TopBar />
           <div
-            className={`fixed  w-60 lg:block hidden transition-all duration-300 h-screen border-r border-gray-400 ${
+            className={`mt-14 ${
               mode ? "bg-black text-slate-300" : "bg-white text-slate-800"
             }`}
           >
-            <Sidebar />
-          </div>
-          <div className="lg:ml-60 ml-0  w-[85.7vw]">
-            <TopBar />
-            <div
-              className={`mt-14 ${
-                mode ? "bg-black text-slate-300" : "bg-white text-slate-800"
-              }`}
-            >
-              <div className="">
-                <div className="pt-10 pb-6">
-                  <div className="flex border rounded-md mx-5">
-                    <div className="w-56 border-r">
-                      <Button>Calender</Button>
-                    </div>
-                    <div className="w-full">
-                      <div className="flex items-center border-b py-3 pl-5 w-full gap-3">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6 cursor-pointer"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                          />
-                        </svg>
+            <div className="pt-10 pb-6">
+              <div className="flex border rounded-md mx-5">
+                <div className="w-56 border-r">
+                  <Button>Compose</Button>
+                </div>
+                <div className="w-full">
+                  <div className="flex items-center border-b py-3 pl-5 w-full gap-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 cursor-pointer"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                      />
+                    </svg>
 
-                        <input
-                          type="text"
-                          placeholder="Search email"
-                          className="  outline-none bg-transparent"
-                        />
-                      </div>
-                      <div className="overflow-y-scroll h-[85vh] scroll-style">
-                      <ul className="">
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        <li className="border-b py-3 px-5">44</li>
-                        
-                        
-                      </ul>
-                      </div>
-                    </div>
+                    <input
+                      type="text"
+                      placeholder="Search email"
+                      className="  outline-none bg-transparent"
+                    />
+                  </div>
+                  <div className="overflow-y-scroll h-[85vh] scroll-style">
+                    <ul className="">
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                      <li className="border-b py-3 px-5">44</li>
+                    </ul>
                   </div>
                 </div>
               </div>
