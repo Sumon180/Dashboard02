59e31879cad3ed9695db1aaf5aaadfc962853e6b
sumon-hossain <sumonengr.96@gmail.com>
2023-03-16 10:33:58 +0600

    WIP

--- src/components/UserListMain.tsx
+++ src/components/UserListMain.tsx
@@ -20,11 +20,139 @@ const UserListMain = () => {
         }`}
       >
         <div className="pt-10 pb-6">
-          <div className="flex flex-wrap">
-            <div className="mx-5 mb-5 w-[20%] min-w-[15rem] h-36 bg-zinc-500 rounded-lg"></div>
-            <div className="mx-5 mb-5 w-[20%] min-w-[15rem] h-36 bg-zinc-500 rounded-lg"></div>
-            <div className="mx-5 mb-5 w-[20%] min-w-[15rem] h-36 bg-zinc-500 rounded-lg"></div>
-            <div className="mx-5 mb-5 w-[20%] min-w-[15rem] h-36 bg-zinc-500 rounded-lg"></div>
+          <div className="flex flex-wrap justify-between">
+            <div
+              className={`${
+                mode
+                  ? "bg-slate-800 text-slate-300"
+                  : "bg-slate-200 text-slate-800"
+              } mx-5 mb-7 rounded-lg w-[20%] min-w-[15rem] h-36 `}
+            >
+              <div className="flex justify-between p-5">
+                <div>
+                  <p>Session</p>
+                  <p className="text-2xl">
+                    21,459 <span className="text-lg">29%</span>
+                  </p>
+                  <p>Total Users</p>
+                </div>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`${
+                mode
+                  ? "bg-slate-800 text-slate-300"
+                  : "bg-slate-200 text-slate-800"
+              } mx-5 mb-5 w-[20%] min-w-[15rem] h-36 rounded-lg`}
+            >
+              <div className="flex justify-between p-5">
+                <div>
+                  <p>Paid Users</p>
+                  <p className="text-2xl">
+                    21,459 <span className="text-lg">29%</span>
+                  </p>
+                  <p>Total Users</p>
+                </div>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`${
+                mode
+                  ? "bg-slate-800 text-slate-300"
+                  : "bg-slate-200 text-slate-800"
+              } mx-5 mb-5 w-[20%] min-w-[15rem] h-36 rounded-lg`}
+            >
+              <div className="flex justify-between p-5">
+                <div>
+                  <p>Active Users</p>
+                  <p className="text-2xl">
+                    21,459 <span className="text-lg">29%</span>
+                  </p>
+                  <p>Total Users</p>
+                </div>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`${
+                mode
+                  ? "bg-slate-800 text-slate-300"
+                  : "bg-slate-200 text-slate-800"
+              } mx-5 mb-5 w-[20%] min-w-[15rem] h-36 rounded-lg`}
+            >
+              <div className="flex justify-between p-5">
+                <div>
+                  <p>Pending Users</p>
+                  <p className="text-2xl">
+                    21,459 <span className="text-lg">29%</span>
+                  </p>
+                  <p>Total Users</p>
+                </div>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="flex flex-col border rounded-md mx-5">
             <div className="">
