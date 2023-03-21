87650a1aa1bc8e62827a6ba214d004a62708aa3e
sumon-hossain <sumonengr.96@gmail.com>
2023-03-21 19:00:37 +0600

    FAQ Done

--- src/components/FAQmain.tsx
+++ src/components/FAQmain.tsx
@@ -52,7 +52,7 @@ const FAQmain = () => {
                     </button>
                   </div>
                   <div>
-                    <img src={register} alt="" />
+                    <img src={register} alt="" className="w-96 h-96" />
                   </div>
                 </div>
                 <div className="w-8/12">
@@ -346,21 +346,51 @@ const FAQmain = () => {
               </div>
 
               <div className="flex items-center flex-col justify-center gap-3 my-24">
+                <span className=" bg-green-900 text-green-500 px-5 py-2">QUESTION?</span>
                 <h6 className="text-2xl">Still need help?</h6>
-
-                <p>
-                  Our specialists are always happy to help. Contact us during
-                  standard business hours or email us
+                <p className="mb-5">
+                  If you can't find question in our FAQ, you can contact us.
+                  We'll answer you shortly!
                 </p>
-                <p>24/7 and we'll get back to you.</p>
+                <div className="flex items-center gap-5 w-full">
+                  <div className="flex flex-col items-center justify-center gap-3 py-10 bg-slate-800 px-5 w-1/2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                      />
+                    </svg>
 
-                <div className="flex items-center gap-5">
-                  <button className="uppercase bg-blue-600 px-5 py-2">
-                    visit our community
-                  </button>
-                  <button className="uppercase bg-blue-600 px-5 py-2">
-                    Contact Us
-                  </button>
+                    <p>+ (810) 2548 2568</p>
+                    <p>We are always happy to help!</p>
+                  </div>
+                  <div className="flex flex-col items-center justify-center gap-3 py-10 bg-slate-800 px-5 w-1/2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                      />
+                    </svg>
+
+                    <p>hello@help.com</p>
+                    <p>Best way to get answer faster!</p>
+                  </div>
                 </div>
               </div>
             </div>
